88a26c64e4e743118f6ba2ef1211042fcdc951cd
Martin von Gagern <gagern@ma.tum.de>
2015-12-03 07:15:13 +0100

    Add top bar

--- metalscript.js
+++ metalscript.js
@@ -7,6 +7,8 @@ var Metalsmith = require("metalsmith");
 var layouts = require("metalsmith-layouts");
 var markdown = require("metalsmith-markdown");
 var nuv = require("./lib/metalsmith-validator-nu");
+var menuCombiner = require("./lib/metalsmith-menu-combiner");
+var menuTopbar = require("./lib/metalsmith-foundation-topbar");
 
 if (require.main === module)
   process.nextTick(main);
@@ -18,6 +20,8 @@ function main() {
 function build(nuv) {
   Metalsmith(__dirname)
     .use(markdown())
+    .use(menuCombiner())
+    .use(menuTopbar())
     .use(layouts({
       engine: "handlebars",
       default: "main.html"
--- serve.js
+++ serve.js
@@ -5,5 +5,7 @@ var app = express();
 
 app.use(express.static("build"));
 app.use(express.static("foundation"));
+app.use("/images", express.static("images"));
 
+console.log("http://localhost:44849/");
 app.listen(44849);
