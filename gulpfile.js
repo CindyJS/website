cede93014f1c671279bae2f3a2f494856497d207
Martin von Gagern <gagern@ma.tum.de>
2016-07-20 23:00:48 +0200

    More cleanup: get rid of bower, try install from fresh checkout

--- gulpfile.js
+++ gulpfile.js
@@ -41,35 +41,8 @@ var PATHS = {
     '!src/assets/{img,js,scss}/**/*'
   ],
   sass: [
-    'bower_components/foundation-sites/scss',
-    'bower_components/motion-ui/src/'
   ],
   javascript: [
-    'bower_components/jquery/dist/jquery.js',
-    'bower_components/what-input/what-input.js',
-    'bower_components/foundation-sites/js/foundation.core.js',
-    'bower_components/foundation-sites/js/foundation.util.*.js',
-    // Paths to individual JS components defined below
-    'bower_components/foundation-sites/js/foundation.abide.js',
-    'bower_components/foundation-sites/js/foundation.accordion.js',
-    'bower_components/foundation-sites/js/foundation.accordionMenu.js',
-    'bower_components/foundation-sites/js/foundation.drilldown.js',
-    'bower_components/foundation-sites/js/foundation.dropdown.js',
-    'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
-    'bower_components/foundation-sites/js/foundation.equalizer.js',
-    'bower_components/foundation-sites/js/foundation.interchange.js',
-    'bower_components/foundation-sites/js/foundation.magellan.js',
-    'bower_components/foundation-sites/js/foundation.offcanvas.js',
-    'bower_components/foundation-sites/js/foundation.orbit.js',
-    'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
-    'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
-    'bower_components/foundation-sites/js/foundation.reveal.js',
-    'bower_components/foundation-sites/js/foundation.slider.js',
-    'bower_components/foundation-sites/js/foundation.sticky.js',
-    'bower_components/foundation-sites/js/foundation.tabs.js',
-    'bower_components/foundation-sites/js/foundation.toggler.js',
-    'bower_components/foundation-sites/js/foundation.tooltip.js',
-    'src/assets/js/!(app.js)**/*.js',
     'src/assets/js/app.js'
   ]
 };
@@ -99,10 +72,6 @@ handlebars.registerHelper("json", function(data) {
             .replace(/</g, "\\x3c")
     );
 });
-
-gulp.task("bower", function() {
-    return cmd.unlessExists("bower_components", ["bower", "install"]);
-}); 
 
 gulp.task("cjsmod", function() {
     return cmd.unlessExists("CindyJS", ["git", "submodule", "update", "--init", "CindyJS"]);
@@ -285,9 +254,7 @@ gulp.task('server', ['rebuild'], function() {
         rewriteRules: [
             {
                 match: /(['"])(\/dist\/)/g,
-                fn: function(match) {
-                    return match[0] + "http://cindyjs.org" + match.substr(1);
-                }
+                replace: "$1http://cindyjs.org$2"
             }
         ],
     });
