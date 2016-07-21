b2f9023d350e89c4c5fe2da7fcc7bf7175538a0e
Martin von Gagern <gagern@ma.tum.de>
2016-07-21 18:24:36 +0200

    Auto-deploy website on commit to master

---
+++ tools/travis-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+umask 0077
+openssl aes-256-cbc -K "${encrypted_e7f1c3b6092d_key}" -iv "${encrypted_e7f1c3b6092d_iv}" \
+    -in tools/travis-ssh.enc -out travis-ssh -d
+umask 0022
+set -x
+eval "$(ssh-agent -s)"
+ssh-add travis-ssh
+rm travis-ssh
+
+mkdir -p "${HOME}/.ssh"
+cat tools/cindyjs.org.pub >> "${HOME}/.ssh/known_hosts"
+rsync --delete-delay -rci --rsh='ssh -l travis' dist/ cindyjs.org::website/
