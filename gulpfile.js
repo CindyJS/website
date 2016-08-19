486e6edbbc652d2fd601debc33f5edf4449b7681
Aaron Montag <montag@ma.tum.de>
2016-08-19 16:44:40 +0200

    now regenerate license-stream whenever needed

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ var Galleries = [{
     description: "This shows the examples <a href='https://github.com/CindyJS/CindyJS'>from the repository</a>, demonstrating individual functions and operations. Most of them demonstrate a single technical feature and are not intended to be examples of what well-designed CindyJS widgets can look like.",
     imgpath: "/assets/img/thumbnail/",
     github: "CindyJS/CindyJS",
-    license: licenses.apache2(),
+    license: licenses.apache2,
 }];
 
 
@@ -151,7 +151,7 @@ gulp.task("pages", ["cjsdeps", "copyexampleimages", "copygallerydata"], function
                         addData(gallerynavigation(gallery.dest)),
                         github(gallery.github || "CindyJS/website"),
                         index(gallery.dest, "src/layouts/gallery.html", xtend(gallery, gallerynavigation(gallery.dest))),
-                        (gallery.license ||  licenses.ccbysa40())
+                        (gallery.license ||  licenses.ccbysa40)()
                     )
                 )
             ),
