a9953a00d7273b7d7e5d1ed9791e2f92ee7103be
Aaron Montag <montag@ma.tum.de>
2016-10-05 11:10:34 +0200

    Add script to generate (most) of the thumbnails
    This script requires phantom.js and imagemagic

---
+++ src/assets/img/thumbnail/cindyscreenshot.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var system = require('system');
+var fs = require('fs');
+
+if (system.args.length < 2) {
+    console.log('Usage: phantomjs gallery.js FILE[s]');
+    phantom.exit(1);
+}
+
+var queue = [];
+
+for (var x = 1; x < system.args.length; x++)
+    if (system.args[x].split('.').pop() == 'html') {
+        var path = system.args[x];
+        var output = /.*\/(.*?)\./.exec(path)[1] + '.png';
+        if (!fs.exists(output)) {
+            queue.push([path, output]);
+        }
+    }
+
+    function emptyqueue() {
+      console.log('queue.length: ' + queue.length);
+      if(queue.length == 0) phantom.exit();
+      else {
+        var cur = queue.pop();
+        renderimage(cur[0], cur[1])
+      }
+    }
+    
+emptyqueue();
+    
+function renderimage(address, output) {
+    var page = require('webpage').create();
+    page.viewportSize = {
+        width: 2000,
+        height: 2000
+    };
+    page.paperSize = {
+        width: 2000,
+        height: 2000,
+        border: '0px'
+    }
+    page.open(address, function(status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+            phantom.exit(1);
+        } else {
+            page.sendEvent('mousemove', 200, 200);
+            page.sendEvent('mouseclick', 200, 200);
+
+            window.setTimeout(function() {
+                page.sendEvent('mousemove', 200, 200);
+                page.sendEvent('mouseclick', 200, 200);
+                var rect = page.evaluate(function() {
+                    var div = document.getElementById('Cindy3D') || document.getElementById('CSCanvas') || document.getElementById('CSCanvas1') || document.getElementById('cindy') || document.getElementById('enCindy');
+                    return div.getBoundingClientRect();
+                });
+
+                page.clipRect = rect ? {
+                    top: rect.top + 2,
+                    left: rect.left + 2,
+                    width: rect.width - 4,
+                    height: rect.height - 4
+                } : {
+                    top: 100,
+                    left: 100,
+                    width: 300,
+                    height: 300
+                };
+                
+                console.log("rendering " + output);
+                page.render(output);
+                
+                emptyqueue();
+            }, 1000);
+        }
+    });
+}
